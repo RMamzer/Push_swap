3a1e8036457d641b3a26878dfc39d55acffe0da2
Roman <rmamzer@student.hive.fi>
2025-06-05 19:51:12 +0300

    errors functions

--- main.c
+++ main.c
@@ -6,13 +6,51 @@
 /*   By: rmamzer <rmamzer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 17:31:25 by rmamzer           #+#    #+#             */
-/*   Updated: 2025/06/05 16:38:46 by rmamzer          ###   ########.fr       */
+/*   Updated: 2025/06/05 19:50:46 by rmamzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include "libft/libft.h"
 //#include "libft.h"
-//function to write error
+//functions to write error
+
+
+
+
+int check_only_digits(char *str)
+{
+	
+}
+
+void errors_exit(t_node **a)
+{
+	//free the node a
+	//add ft_printf
+	ft_printf("Error\n");
+	exit (1);
+}
+
+create_stack_a (t_node **a, char **argv)
+{
+	// check that its a num;
+	// check that its within int
+	// check that there are no duplicates before
+
+	int num;
+	int i;
+
+	num = 0;
+	i = 0;
+	while (argv[i])
+	{
+		if(!check_only_digits(argv[i]))
+			errors_exit(a);
+	return (0);
+	}
+	
+}
+
 
 int main(int argc, char ** argv)
 {
@@ -23,7 +61,10 @@ int main(int argc, char ** argv)
 		return (1);
 	a = NULL;
 	b = NULL;
+	if (argc == 2)
+		argv = ft_split(argv[1], " ");
 	
+	create_stack_a(&a, argv + 1);
 	return (0);
 	
 }
