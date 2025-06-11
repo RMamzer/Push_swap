ae2c85d1b86750af7f60b15d4be807b3d68aae15
Roman Mamzer <mamzer.roman@gmail.com>
2025-06-11 18:29:50 +0300

    completed operations functionality

--- push_operations.c
+++ push_operations.c
@@ -6,44 +6,19 @@
 /*   By: rmamzer <rmamzer@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 14:09:57 by rmamzer           #+#    #+#             */
-/*   Updated: 2025/06/11 15:00:23 by rmamzer          ###   ########.fr       */
+/*   Updated: 2025/06/11 18:29:11 by rmamzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/*
-sa (swap a): Swap the first 2 elements at the top of stack a.
-Do nothing if there is only one element or none.
-sb (swap b): Swap the first 2 elements at the top of stack b.
-Do nothing if there is only one element or none.
-ss : sa and sb at the same time.
-pa (push a): Take the first element at the top of b and put it at the top of a.
-Do nothing if b is empty.
-pb (push b): Take the first element at the top of a and put it at the top of b.
-Do nothing if a is empty.
-ra (rotate a): Shift up all elements of stack a by 1.
-The first element becomes the last one.
-rb (rotate b): Shift up all elements of stack b by 1.
-The first element becomes the last one.
-rr : ra and rb at the same time.
-rra (reverse rotate a): Shift down all elements of stack a by 1.
-The last element becomes the first one.
-rrb (reverse rotate b): Shift down all elements of stack b by 1.
-The last element becomes the first one.
-rrr : rra and rrb at the same time.*/
 
-
-/*
-what to do in push:
-1.get the node
-2. move src from node to the next --> check if previous null
-
-
-*/
 void	push_operation(t_node **dest,t_node **src)
 {
 	t_node	*node;
+
+	if (*src == NULL)
+		return ;
 	node = *src;
 	*src = (*src)->next;
 
@@ -75,4 +50,73 @@ void	pb(t_node **a, t_node **b)
 {
 	push_operation(b,a);
 	write(1, "pb\n", 3);
+}
+
+
+void	rotate_operation(t_node **stack)
+{
+	t_node	*first;
+	t_node	*last;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
+	first = *stack;
+	last = get_last_node(*stack);
+
+	*stack = first->next;
+	(*stack)-> previous = NULL;
+	first->previous = last;
+	first ->next = NULL;
+	last->next = first;
+}
+
+void	ra(t_node **a)
+{
+	rotate_operation(a);
+	write (1, "ra\n", 3);
+}
+void	rb(t_node **b)
+{
+	rotate_operation(b);
+	write (1, "rb\n", 3);
+}
+
+void	rr(t_node **a, t_node **b)
+{
+	rotate_operation(a);
+	rotate_operation(b);
+	write (1, "rr\n", 3);
+}
+
+void	reverse_rotate_operation(t_node **stack)
+{
+	t_node	*last;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
+	last = get_last_node(*stack);
+	last->previous->next = NULL;
+	last->previous = NULL;
+	last->next =*stack;
+	*stack = last;
+	last->next->previous = last;
+}
+
+void rra (t_node **a)
+{
+	reverse_rotate_operation(a);
+	write (1, "rra\n", 4);
+}
+
+void rrb (t_node **b)
+{
+	reverse_rotate_operation(b);
+	write (1, "rrb\n", 4);
+}
+
+void rrr (t_node **a, t_node **b)
+{
+	reverse_rotate_operation(a);
+	reverse_rotate_operation(b);
+	write (1, "rrr\n", 4);
 }
---
+++ swap_operations.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap_operations.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmamzer <rmamzer@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 17:53:19 by rmamzer           #+#    #+#             */
+/*   Updated: 2025/06/11 17:53:47 by rmamzer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	swap_operation(t_node **stack)
+{
+	t_node	*first;
+	t_node	*second;
+
+	if (*stack == NULL || ((*stack)->next == NULL))
+		return ;
+	first = *stack;
+	second = (*stack)->next;
+	first->next = second->next;
+	second->next = first;
+	second->previous = NULL;
+	first->previous = second;
+}
+
+void	sa(t_node **a)
+{
+	swap_operation(a);
+	write (1, "sa\n", 3);
+}
+void	sb(t_node **b)
+{
+	swap_operation(b);
+	write (1, "sb\n", 3);
+}
+void ss(t_node **a, t_node ** b)
+{
+	swap_operation(a);
+	swap_operation(b);
+	write (1, "ss\n", 3);
+}
